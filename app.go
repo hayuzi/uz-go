55fb7e81f7088fa876265972f15f4c416663801b
yuzi <hayuzi@163.com>
2018-12-28 09:35:01 +0800

    init

--- app.go
+++ app.go
@@ -1,1 +1,35 @@
 package uzgo
+
+import (
+	"log"
+	"net"
+	"fmt"
+)
+
+func main() {
+
+	// 启动
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+
+}
+
+func broadcaster() {
+
+}
+
+func handleConn(conn) {
+	fmt.Println(conn)
+}
