ce7619db99bed56813a9fbdcbaf74d4cb0601714
yuzi <hayuzi@163.com>
2018-12-29 17:40:59 +0800

    init

--- app.go
+++ app.go
@@ -30,18 +30,31 @@ func main() {
 type client chan<- string // 对外发送消息的通道
 var (
 	entering = make(chan client)
-	leafing  = make(chan client)
+	leaving  = make(chan client)
 	messages = make(chan string) // 所有接收的客户消息
 )
 
 func broadcaster() {
 	clients := make(map[client]bool) // 所有连接的客户端
 	for {
-		select {}
+		select {
+		case msg := <-messages:
+			// 把所有接收的消息广播给所有的客户
+			// 发送消息通道
+			for cli := range clients {
+				cli <- msg
+			}
+		case cli := <-entering:
+			clients[cli] = true
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli)
+		}
 	}
 
 }
 
 func handleConn(conn net.Conn) {
 	fmt.Println(conn)
+
 }
